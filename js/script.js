c0f642e5282fc95edb81c5c1b4a88b47932a61b0
Yurbus <frankins636@gmail.com>
2025-04-16 17:07:10 +0300

    dsds

--- js/script.js
+++ js/script.js
@@ -65,20 +65,33 @@ const isMobile = {
 // }
 
 // -------- Accordion -----------------------------------------
-var acc = document.getElementsByClassName("accordion");
-var i;
+// var acc = document.getElementsByClassName("accordion");
+// var i;
 
-    for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function() {
-        this.classList.toggle("active");
-        var panel = this.nextElementSibling;
-        if (panel.style.maxHeight) {
-        panel.style.maxHeight = null;
-        } else {
-        panel.style.maxHeight = panel.scrollHeight + "px";
-        }
+//     for (i = 0; i < acc.length; i++) {
+//     acc[i].addEventListener("click", function() {
+//         this.classList.toggle("active");
+//         var panel = this.nextElementSibling;
+//         if (panel.style.maxHeight) {
+//         panel.style.maxHeight = null;
+//         } else {
+//         panel.style.maxHeight = panel.scrollHeight + "px";
+//         }
+//     });
+// }
+
+document.querySelectorAll('.accordion-title').forEach(title => {
+    title.addEventListener('click', () => {
+      const item = title.parentElement;
+      const isActive = item.classList.contains('active');
+
+      document.querySelectorAll('.accordion-item').forEach(i => i.classList.remove('active'));
+
+      if (!isActive) {
+        item.classList.add('active');
+      }
     });
-}
+  });
 
 
 // --------- header------------------------------------------------
