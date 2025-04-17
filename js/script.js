d51443305d7bb12c2c674363d7cac5a5fb233ecd
Yurbus <frankins636@gmail.com>
2025-04-17 11:24:49 +0300

    ккккк

--- js/script.js
+++ js/script.js
@@ -82,16 +82,16 @@ const isMobile = {
 
 document.querySelectorAll('.accordion-title').forEach(title => {
     title.addEventListener('click', () => {
-      const item = title.parentElement;
-      const isActive = item.classList.contains('active');
+    const item = title.parentElement;
+    const isActive = item.classList.contains('active');
 
-      document.querySelectorAll('.accordion-item').forEach(i => i.classList.remove('active'));
+        document.querySelectorAll('.accordion-item').forEach(i => i.classList.remove('active'));
 
-      if (!isActive) {
-        item.classList.add('active');
-      }
+        if (!isActive) {
+            item.classList.add('active');
+        }
     });
-  });
+});
 
 
 // --------- header------------------------------------------------
